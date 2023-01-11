11643fa11e18fa232c8b56c5853c28d7178db7f6
Tinashe Gutu <tinashegutu2011@gmail.com>
2023-01-11 15:15:13 +0000

    add useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import HomePage, { BreedData } from "./components/HomePage";
 import Leaderboard from "./components/LeaderBoard";
 import NavBar from "./components/NavBar";
 import { url } from "./components/HomePage";
 import { randomiseBreed } from "./utils/randomiseBreed";
-import "./App.css"
+import "./App.css";
 
 function App(): JSX.Element {
   const [allBreeds, setAllBreeds] = useState<string[]>([]);
@@ -13,12 +13,7 @@ function App(): JSX.Element {
   const [image2, setImage2] = useState("");
   const [view, setView] = useState<"VotePage" | "LeaderboardPage">("VotePage");
 
-  useEffect(() => {
-    fetchAllData();
-  }, []);
-
-  //get all data - will run on every initial launch
-  async function fetchAllData() {
+  const fetchAllData = useCallback(async () => {
     const fetchedData = await axios.get(`${url}/dogs/breeds`);
     const allData = fetchedData.data;
     const breedNames = allData.map((breedData: BreedData) => {
@@ -27,7 +22,13 @@ function App(): JSX.Element {
     setAllBreeds(breedNames);
     console.log(breedNames);
     fetchAndStoreImages(breedNames);
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchAllData();
+  }, [fetchAllData]);
+
+  //get all data - will run on every initial launch
 
   //take 2 random breeds and make sure they are different-> breeds, breed[0], breed[1], randomiseBreed()[0]
   async function fetchAndStoreImages(breedNames: string[]) {
