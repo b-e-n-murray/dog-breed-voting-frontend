cf0f3270cb2a700635f7179b2717e662edb0930c
Tinashe Gutu <tinashegutu2011@gmail.com>
2023-01-11 16:51:44 +0000

    render two new dogs when image is clicked

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,6 +1,11 @@
-export const url = "https://dog-breed-voting-backend.onrender.com";
-//  : 'https://localhost:4000'
-//  process.env.NODE_ENV === "production" ?
+import axios from "axios";
+import { Image } from "../App";
+
+export const url =
+  process.env.NODE_ENV === "production"
+    ? "https://dog-breed-voting-backend.onrender.com"
+    : "http://localhost:4000";
+
 export interface BreedData {
   id: number;
   breedname: string;
@@ -9,20 +14,31 @@ export interface BreedData {
 
 interface HomePageProps {
   fetchAndStoreImages: () => Promise<void>;
-  image1: string;
-  image2: string;
+  image1: Image;
+  image2: Image;
 }
 function HomePage({
   fetchAndStoreImages,
   image1,
   image2,
 }: HomePageProps): JSX.Element {
+  async function submitVote(ImageBreedName: string) {
+    console.log("calling submit vote, breed:", ImageBreedName);
+    await axios.patch(`${url}/dogs/breed/vote`, { breedname: ImageBreedName });
+    fetchAndStoreImages();
+  }
+
   return (
     <>
       <h1>Dog Breed Voting App</h1>
-      <button onClick={fetchAndStoreImages}>Get dog</button>
-      <img src={image1} alt="random dog" className="voteImage"></img>
-      <img src={image2} alt="random dog" className="voteImage"></img>
+      <div onClick={() => submitVote(image1.breedname)} className="image1">
+        <img src={image1.url} alt="random dog" className="voteImage"></img>
+      </div>
+      <h4>{image1.breedname}</h4>
+      <div onClick={() => submitVote(image2.breedname)} className="image2">
+        <img src={image2.url} alt="random dog" className="voteImage"></img>
+      </div>
+      <h4>{image2.breedname}</h4>
     </>
   );
 }
