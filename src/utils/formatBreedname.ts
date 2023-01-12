99f9eb5680a462823f3b95f896d25b10d6b035dc
Maria <mtens17@gmail.com>
2023-01-12 11:58:08 +0000

    fix formatbreedname

--- src/utils/formatBreedname.ts
+++ src/utils/formatBreedname.ts
@@ -1,11 +1,11 @@
 export function formatBreedname(breedname: string): string{
     if (breedname.includes("/")){
-        let breednameArr = breedname.split("/")
-        let secondWord = breednameArr[0][0].toUpperCase() + breednameArr[0].slice(1)
-        let firstWord = breednameArr[1][0].toUpperCase()+ breednameArr[1].slice(1)
+        const breednameArr = breedname.split("/")
+        const secondWord = breednameArr[0][0].toUpperCase() + breednameArr[0].slice(1)
+        const firstWord = breednameArr[1][0].toUpperCase()+ breednameArr[1].slice(1)
         return `${firstWord} ${secondWord}`
         
         
     }
-    return breedname.toUpperCase()
+    return breedname[0].toUpperCase() + breedname.slice(1)
 }
