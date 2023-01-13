7d42bf52ed8ed55d63ac037b2e5108a96ab194a4
Tinashe Gutu <tinashegutu2011@gmail.com>
2023-01-13 14:23:11 +0000

    change test spicificity

--- src/utils/formatBreedname.test.ts
+++ src/utils/formatBreedname.test.ts
@@ -4,6 +4,6 @@ test("formatBreedname returns the words in reverse order and without the slash",
   expect(formatBreedname("shepherd/german")).toBe("German Shepherd");
 });
 
-test("formatBreedname returns the words in reverse order and without the slash", () => {
-  expect(formatBreedname("shepherd")).toBe("SHEPHERD");
+test("formatBreedname returns the words with first letter capitalised", () => {
+  expect(formatBreedname("shepherd")).toBe("Shepherd");
 });
